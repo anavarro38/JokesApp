78a028528e092525cd1b61fbb21baa5b3752cdf0
anavarro38 <91505302+anavarro38@users.noreply.github.com>
2023-06-16 18:03:13 -0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -4,10 +4,10 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 // modify these settings according to the account on your database server.
-$host = "localhost:8889";
-$username = "root";
-$password = "root";
-$database = "JokesDB";
+$host = "jdbc:mysql://tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
+$username = "nj9nocakyktpg9sa";
+$password = "uxk17ywa5puti7xx";
+$database = "r1h4aq3o587hzts9";
 
 
 $mysqli = new mysqli($host, $username, $password, $database);
