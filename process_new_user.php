c7ede5c81f945bcf0490665c859db86429126e86
anavarro38 <91505302+anavarro38@users.noreply.github.com>
2023-06-16 17:52:09 -0700

    Add files via upload

---
+++ process_new_user.php
@@ -0,0 +1,49 @@
+<?php
+error_reporting(E_ALL);ini_set('display_errors', 1);
+include "db_connect.php";
+$new_username = $_GET['username'];
+$new_password = $_GET['password']; //might not work
+$new_password1 = $_GET['password'];
+$new_password2 = $_GET['password-confirm']; 
+
+$hashed_password = password_hash($new_password1, PASSWORD_DEFAULT);
+
+echo "<h2>Trying to add a new user " . $new_username . " pw =  " . $new_password1 . " and " . $new_password2 . "</h2>";
+
+$sql = "SELECT * FROM users WHERE user_name = '$new_username'";
+$result = $mysqli->query($sql) or die (mysqli_error($mysqli));
+
+$hashed_password = password_hash($new_password1, PASSWORD_DEFAULT);
+
+if ($result->num_rows > 0) {
+    echo "The username " . $new_username . " is already in use.  Try another.";
+    exit;
+} else if ($new_password1 != $new_password2) {
+    echo "The passwords do not match. Please try again.";
+    exit;
+} else {
+    preg_match('/[0-9]+/', $new_password1, $matches);
+    if (sizeof($matches) == 0) {
+        echo "The password must have at least one number<br>";
+        exit;
+    } else {
+        preg_match('/[!@#$%^&*()]+/', $new_password1, $matches);
+        if (sizeof($matches) == 0) {
+            echo "The password must have at least one of these characters: !@#$%^&*()<br>";
+            exit;
+        } else if (strlen($new_password1) <= 8) {
+            echo "The password needs to be at least 8 characters long";
+            exit;
+        } else {
+            $sql = "INSERT INTO users (user_id, user_name, user_pass) VALUES (null, '$new_username', '$hashed_password')";
+            $result = $mysqli->query($sql) or die(mysqli_error($mysqli));
+            if ($result) {
+                echo "Registration success!";
+            } else {
+                echo "Something went wrong. Not registered.";
+            }
+        }
+    }
+}
+
+echo "<a href='index.php'>Return to main</a>";
